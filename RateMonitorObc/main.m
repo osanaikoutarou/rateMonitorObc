d31800609fe592f06fdd2c71e27e88d56523ece5
osanai <osanai@cr-040.local>
2017-09-04 12:16:33 +0900

    Initial Commit

---
+++ RateMonitorObc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RateMonitorObc
+//
+//  Created by osanai on 2017/09/04.
+//  Copyright © 2017年 osanai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
